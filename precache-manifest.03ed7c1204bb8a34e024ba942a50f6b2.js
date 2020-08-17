f8c6362d469f928083f7a5fd4e153fce2fe41618
Zynwala <filip.zynwala@capgemini.com>
2020-08-17 21:39:40 +0200

    Updates

---
+++ precache-manifest.03ed7c1204bb8a34e024ba942a50f6b2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6dd76280ede98db8fccfa156fa31337",
+    "url": "/shopinglist-frontend/index.html"
+  },
+  {
+    "revision": "a11797df418ee84f6699",
+    "url": "/shopinglist-frontend/static/css/main.73c0d935.chunk.css"
+  },
+  {
+    "revision": "2611b0d1466b6398e405",
+    "url": "/shopinglist-frontend/static/js/2.6769737a.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/shopinglist-frontend/static/js/2.6769737a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a11797df418ee84f6699",
+    "url": "/shopinglist-frontend/static/js/main.57bb39b2.chunk.js"
+  },
+  {
+    "revision": "d2e52bd06ef594334ed5",
+    "url": "/shopinglist-frontend/static/js/runtime-main.43e35650.js"
+  },
+  {
+    "revision": "1221d5a9abcfd5006ea2fc1f808dcef8",
+    "url": "/shopinglist-frontend/static/media/fontello.1221d5a9.eot"
+  },
+  {
+    "revision": "9a98210de1eb5a728daa999afc90c7d2",
+    "url": "/shopinglist-frontend/static/media/fontello.9a98210d.svg"
+  },
+  {
+    "revision": "b47da63abc60f116b393f0a0b75f0a1d",
+    "url": "/shopinglist-frontend/static/media/fontello.b47da63a.woff2"
+  },
+  {
+    "revision": "d203565bd1c280d39d3ce8281b718cf3",
+    "url": "/shopinglist-frontend/static/media/fontello.d203565b.woff"
+  },
+  {
+    "revision": "e509f8d3ee55f5ead75095feb0b5660a",
+    "url": "/shopinglist-frontend/static/media/fontello.e509f8d3.ttf"
+  }
+]);
