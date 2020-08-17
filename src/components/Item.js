74def5ddc9d932bec51bafc5fd4cdac59631772e
Zynwala <filip.zynwala@capgemini.com>
2020-08-17 17:29:14 +0200

    Add delete button

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,6 +1,6 @@
 import '../style/item.css'
 import React from 'react';
-import { editItem } from '../actions/index';
+import { editItem, deleteItem } from '../actions/index';
 import { connect } from 'react-redux';
 
 class Item extends React.Component {
@@ -9,16 +9,24 @@ class Item extends React.Component {
         await this.props.editItem(editedItem);
     }
 
+    onClickDelete = async (event) => {
+        event.stopPropagation();
+        await this.props.deleteItem(this.props.item._id);
+    }
+
     render() {
         let status = this.props.item.isDone ? 'done' : 'pending';
         return (
             <React.Fragment>
                 <li className={`item ${status}`} onClick={this.onClickItem}>
                     {this.props.item.content}
+                    <div onClick={this.onClickDelete} className="button-delete">
+                        <i className="icon-cancel-circled" />
+                    </div>
                 </li>
             </React.Fragment>
         );
     };
 };
 
-export default connect(null, { editItem })(Item);
+export default connect(null, { editItem, deleteItem })(Item);
