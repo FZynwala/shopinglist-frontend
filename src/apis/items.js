0fcbf2707e06a192a99bd3061f4ae4fc6410e345
Zynwala <filip.zynwala@capgemini.com>
2020-08-18 21:06:28 +0200

    change api

--- src/apis/items.js
+++ src/apis/items.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const urlExpress = 'http://localhost:8000';
+const urlHeroku = 'https://fierce-fortress-14005.herokuapp.com/';
 
 export default axios.create({
-    baseURL: urlExpress
+    baseURL: urlHeroku
 });
